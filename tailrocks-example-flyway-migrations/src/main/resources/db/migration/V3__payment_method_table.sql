0088e627dabf75d782a5eff0d899ca0bee121cfc
Alexey Zhokhov <alexey@zhokhov.com>
2021-07-10 03:10:05 +0800

    Length fixes.

--- tailrocks-example-flyway-migrations/src/main/resources/db/migration/V3__payment_method_table.sql
+++ tailrocks-example-flyway-migrations/src/main/resources/db/migration/V3__payment_method_table.sql
@@ -1,12 +1,12 @@
 CREATE TYPE "payment_method_card_brand" AS ENUM ('VISA', 'MASTERCARD');
 
 CREATE TABLE payment_method (
-    id                   CHAR(25)                                  NOT NULL,
+    id                   CHAR(24)                                  NOT NULL,
     created_date         TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
     last_modified_date   TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
     version              BIGINT                      DEFAULT 0     NOT NULL,
 
-    account_id           CHAR(25)                                  NOT NULL,
+    account_id           CHAR(24)                                  NOT NULL,
     card_brand           PAYMENT_METHOD_CARD_BRAND,
     card_expiration_date DATE,
     card_number          CHARACTER VARYING(255),
