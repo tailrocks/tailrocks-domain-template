f4cea544f2dd5841af7f0922ddecff7141218488
Alexey Zhokhov <alexey@zhokhov.com>
2021-07-09 21:16:48 +0800

    Switch to use ObjectID instead of BIGINT.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,23 +2,23 @@ object Versions {
 
     // Languages
 
-    const val kotlin = "1.5.10"
+    const val kotlin = "1.5.20"
 
     // Gradle plugins
 
     // https://plugins.gradle.org/plugin/com.github.ben-manes.versions
-    const val gradleVersionsPlugin = "0.38.0"
+    const val gradleVersionsPlugin = "0.39.0"
 
     // https://plugins.gradle.org/plugin/com.diffplug.spotless
-    const val gradleSpotlessPlugin = "5.12.5"
+    const val gradleSpotlessPlugin = "5.14.1"
 
     // https://plugins.gradle.org/plugin/nu.studer.jooq
-    const val gradleJooqPlugin = "5.2.1"
+    const val gradleJooqPlugin = "6.0"
 
     // Libraries
 
     // sync with Micronaut's BOM:
-    // https://repo1.maven.org/maven2/io/micronaut/micronaut-bom/2.5.5/micronaut-bom-2.5.5.pom
+    // https://repo1.maven.org/maven2/io/micronaut/micronaut-bom/2.5.8/micronaut-bom-2.5.8.pom
     // https://repo1.maven.org/maven2/io/micronaut/sql/micronaut-jooq/3.4.0/micronaut-jooq-3.4.0.pom
     const val postgresql = "42.2.18"
     const val flyway = "7.7.3"
--- publish-local.sh
+++ publish-local.sh
@@ -2,4 +2,4 @@
 ABSOLUTE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "${ABSOLUTE_PATH}" || exit
 
-./gradlew publishToMavenLocal --info
+./gradlew clean publishToMavenLocal --info
